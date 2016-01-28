554026b547a19a4dd2bac6977dc985a0f4ab6ff0
Alex Moser <amoser@leadnomics.com>
2016-01-28 14:44:01 -0500

    TEST: Break Travis build on purpose

--- test/test.js
+++ test/test.js
@@ -1,4 +1,10 @@
 // test.js
+describe('This test', function() {
+	it('shoudl always return true', function() {
+		expect(true).toBe(false);
+	});
+})
+
 describe('Add', function() {
 	it('Should add two numbers together', function() {
 		expect(add(1,2)).toBe(3);
