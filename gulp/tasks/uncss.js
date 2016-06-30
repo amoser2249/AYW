ade64d1b2c379434da27acce3cc760f437d62c69
Alex Moser <amoser@leadnomics.com>
2016-06-30 13:37:46 -0400

    Solar Updates

--- gulp/tasks/uncss.js
+++ gulp/tasks/uncss.js
@@ -8,10 +8,10 @@ var $ = require('gulp-load-plugins')(),
 
 // BrowserSync Server
 gulp.task('uncss', function() {
-	return gulp.src('./app/prod/gmr.css')
+	return gulp.src('./app/css/cmeb.css')
 		.pipe($.uncss({
-			html: ['./app/index.html'],
+			html: ['./app/index.html', './app/about-us.html', './app/aio.html', './app/contact-thanks.html', './app/contact.html', './app/energy-companies.html', './app/exit.html', './app/loader.html', './app/offers.html', './app/privacy.html', './app/terms.html'],
 			ignore: ['@import']
 		}))
-		.pipe(gulp.dest('./app/css/'));
+		.pipe(gulp.dest('./app/prod/'));
 });
