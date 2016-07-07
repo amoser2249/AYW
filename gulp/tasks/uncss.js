07c08f69f250184624e6872698a79b3a376ef436
Alex Moser <amoser@leadnomics.com>
2016-07-07 16:48:08 -0400

    solar

--- gulp/tasks/uncss.js
+++ gulp/tasks/uncss.js
@@ -10,7 +10,7 @@ var $ = require('gulp-load-plugins')(),
 gulp.task('uncss', function() {
 	return gulp.src('./app/css/cmeb.css')
 		.pipe($.uncss({
-			html: ['./app/index.html', './app/about-us.html', './app/aio.html', './app/contact-thanks.html', './app/contact.html', './app/energy-companies.html', './app/exit.html', './app/loader.html', './app/offers.html', './app/privacy.html', './app/terms.html'],
+			html: ['./app/index.html', './app/about-us.html', './app/aio.html', './app/aio2.html', './app/contact-thanks.html', './app/contact.html', './app/energy-companies.html', './app/exit.html', './app/loader.html', './app/offers.html', './app/privacy.html', './app/terms.html'],
 			ignore: ['@import']
 		}))
 		.pipe(gulp.dest('./app/prod/'));
